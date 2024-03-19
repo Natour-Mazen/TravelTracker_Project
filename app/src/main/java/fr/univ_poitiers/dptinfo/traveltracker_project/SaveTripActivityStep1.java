be3d5f6a6fd5a5f0478461d71760d891006bd51a
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-19 18:15:28 +0100

    SaveTrip Steps
    add layouts and activities for the SaveTrip Part

---
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/SaveTripActivityStep1.java
@@ -0,0 +1,24 @@
+package fr.univ_poitiers.dptinfo.traveltracker_project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SaveTripActivityStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_save_trip_step1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

ac4ad6e852c224a749d517f3ef297c8fc0a94fe2
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-19 15:58:00 +0100

    big changes again and again
    finsih signup Layout

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/LoginActivity.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
+
         PreviousButton.setupPreviousButton(this,R.id.buttonPrevious);
 
         Button signin= findViewById(R.id.buttonSignIn);

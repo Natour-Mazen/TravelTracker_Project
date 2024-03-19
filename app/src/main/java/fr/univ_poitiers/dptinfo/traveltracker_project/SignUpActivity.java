dcf92239240dbc4303dd6f6aa51d171c9130b56c
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-19 17:53:28 +0100

    HomePage
    Finish the layout of the homePage

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/SignUpActivity.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/SignUpActivity.java
@@ -2,7 +2,9 @@ package fr.univ_poitiers.dptinfo.traveltracker_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import fr.univ_poitiers.dptinfo.traveltracker_project.Utils.PreviousButton;
 
@@ -14,5 +16,12 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         PreviousButton.setupPreviousButton(this,R.id.buttonPrevious);
+
+        Button cont = findViewById(R.id.buttonCompleteReg);
+        cont.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpActivity.this,HomeActivity.class);
+            startActivity(intent);
+        });
+
     }
 }

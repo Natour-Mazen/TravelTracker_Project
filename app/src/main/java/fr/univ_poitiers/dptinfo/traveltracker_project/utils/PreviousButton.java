e23373cccc66e42f959d66d1a51dda6f3743bacf
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-22 23:53:59 +0100

    Update PreviousButton.java
    add comments

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/PreviousButton.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/PreviousButton.java
@@ -5,12 +5,28 @@ import android.widget.Button;
 import org.jetbrains.annotations.NotNull;
 
 
+import android.app.Activity;
+import android.widget.Button;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Utility class for setting up a "previous" button in an activity.
+ */
 public class PreviousButton {
 
-    public static void setupPreviousButton(@NotNull Activity activity,int ButtonId ) {
-        Button previousButton = activity.findViewById(ButtonId);
+    /**
+     * Sets up a button in the activity to act as a "previous" button.
+     * @param activity The activity where the button is located.
+     * @param buttonId The resource ID of the button.
+     */
+    public static void setupPreviousButton(@NotNull Activity activity, int buttonId) {
+        // Find the button using its resource ID
+        Button previousButton = activity.findViewById(buttonId);
+
+        // Set click listener to finish the current activity when the button is clicked
         previousButton.setOnClickListener(v -> {
-            // finish the current Activity
+            // Finish the current activity
             activity.finish();
         });
     }

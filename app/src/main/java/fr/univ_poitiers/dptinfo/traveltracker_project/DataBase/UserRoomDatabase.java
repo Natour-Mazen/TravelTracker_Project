ef28b63a512abe35fae4ba37a3bbd5d8f6c59bfd
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-22 23:50:46 +0100

    Update UserRoomDatabase.java
    add comments

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/DataBase/UserRoomDatabase.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/DataBase/UserRoomDatabase.java
@@ -11,26 +11,36 @@ import java.util.concurrent.Executors;
 import fr.univ_poitiers.dptinfo.traveltracker_project.DataBase.DAOs.UserDao;
 import fr.univ_poitiers.dptinfo.traveltracker_project.DataBase.entities.User;
 
+/**
+ * Room Database class for handling user-related data operations.
+ */
 @Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class UserRoomDatabase extends RoomDatabase {
     public abstract UserDao userDao();
 
-    private static volatile UserRoomDatabase pokemonRoomDatabase;
+    // ExecutorService for database operations on a separate thread
     private static final int NUMBER_OF_THREADS = 4;
     public static final ExecutorService databaseWriteExecutor =
             Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
+    // Singleton instance of UserRoomDatabase
+    private static volatile UserRoomDatabase userRoomDatabase;
+
+    /**
+     * Retrieves the singleton instance of UserRoomDatabase.
+     * @param context The application context.
+     * @return Singleton instance of UserRoomDatabase.
+     */
     public static UserRoomDatabase getDatabase(final Context context) {
-        if (pokemonRoomDatabase == null) {
+        if (userRoomDatabase == null) {
             synchronized (UserRoomDatabase.class) {
-                if (pokemonRoomDatabase == null) {
-                    pokemonRoomDatabase = Room.databaseBuilder(context.getApplicationContext(),
+                if (userRoomDatabase == null) {
+                    userRoomDatabase = Room.databaseBuilder(context.getApplicationContext(),
                                     UserRoomDatabase.class, "user_database")
                             .build();
                 }
             }
         }
-        return pokemonRoomDatabase;
+        return userRoomDatabase;
     }
 }
-

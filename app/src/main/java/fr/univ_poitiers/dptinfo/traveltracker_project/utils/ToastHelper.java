ee7e723871d5d233dffe8bae6e4f7c7af69cb0ed
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-22 23:54:23 +0100

    Update ToastHelper.java
    add comments

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/ToastHelper.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/ToastHelper.java
@@ -3,16 +3,35 @@ package fr.univ_poitiers.dptinfo.traveltracker_project.utils;
 import android.content.Context;
 import android.widget.Toast;
 
+/**
+ * Utility class for displaying toast messages.
+ */
 public class ToastHelper {
 
+    /**
+     * Displays a toast message with custom duration.
+     * @param context The context to display the toast message.
+     * @param message The message to be displayed.
+     * @param duration The duration of the toast message.
+     */
     public static void showToast(Context context, String message, int duration) {
         Toast.makeText(context, message, duration).show();
     }
 
+    /**
+     * Displays a short toast message.
+     * @param context The context to display the toast message.
+     * @param message The message to be displayed.
+     */
     public static void showShortToast(Context context, String message) {
         showToast(context, message, Toast.LENGTH_SHORT);
     }
 
+    /**
+     * Displays a long toast message.
+     * @param context The context to display the toast message.
+     * @param message The message to be displayed.
+     */
     public static void showLongToast(Context context, String message) {
         showToast(context, message, Toast.LENGTH_LONG);
     }

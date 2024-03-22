623c69e609238d89fdd5bb756a64f334b77c85c6
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-03-22 23:54:54 +0100

    Update LogHelper.java
    add comments

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/LogHelper.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/LogHelper.java
@@ -2,16 +2,34 @@ package fr.univ_poitiers.dptinfo.traveltracker_project.utils;
 
 import android.util.Log;
 
+/**
+ * Utility class for logging messages at different log levels.
+ */
 public class LogHelper {
 
+    /**
+     * Logs an error message.
+     * @param tag The tag to identify the log message.
+     * @param message The error message to be logged.
+     */
     public static void logError(String tag, String message) {
         Log.e(tag, message);
     }
 
+    /**
+     * Logs an informational message.
+     * @param tag The tag to identify the log message.
+     * @param message The informational message to be logged.
+     */
     public static void logInfo(String tag, String message) {
         Log.i(tag, message);
     }
 
+    /**
+     * Logs a debug message.
+     * @param tag The tag to identify the log message.
+     * @param message The debug message to be logged.
+     */
     public static void logDebug(String tag, String message) {
         Log.d(tag, message);
     }

ae94e792cf1e0ebefe4ff14eb495bfac3e81053e
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-04-05 23:58:48 +0200

    Create SeekBarTextViewBinder.java
    add this new classe to handle a seekbar and a textview

---
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/UIHelpers/SeekBarTextViewBinder.java
@@ -0,0 +1,34 @@
+package fr.univ_poitiers.dptinfo.traveltracker_project.utils.UIHelpers;
+
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SeekBarTextViewBinder {
+    private final SeekBar seekBar;
+    private final TextView textView;
+
+    public SeekBarTextViewBinder(@NotNull SeekBar seekBar,@NotNull TextView textView) {
+        this.seekBar = seekBar;
+        this.textView = textView;
+        setupSeekBarListener();
+    }
+
+    private void setupSeekBarListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}

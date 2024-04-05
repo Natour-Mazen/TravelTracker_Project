a692c3510361a79741c1a370cba7b6261d17d4d1
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-04-05 23:57:50 +0200

    Create CalendarViewActivityBinder.java
    add new class to handle calander events

---
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/utils/UIHelpers/CalendarViewActivityBinder.java
@@ -0,0 +1,42 @@
+package fr.univ_poitiers.dptinfo.traveltracker_project.utils.UIHelpers;
+
+import android.widget.CalendarView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import fr.univ_poitiers.dptinfo.traveltracker_project.utils.DataHelpers.LogHelper;
+
+public class CalendarViewActivityBinder {
+    private final CalendarView calendarView;
+    private String selectedDate;
+
+    public CalendarViewActivityBinder(CalendarView calendarView) {
+        this.calendarView = calendarView;
+        setupCalendarViewListener();
+    }
+
+    private void setupCalendarViewListener() {
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            // Increment month because Calendar month is zero-based
+            month++;
+
+            // Create a Calendar instance and set the selected date
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month - 1, dayOfMonth);
+
+            // Convert Calendar to Date
+            Date selectedDate = calendar.getTime();
+
+            // Format the Date as a string
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+            this.selectedDate = dateFormat.format(selectedDate);
+
+        });
+    }
+
+    public String getSelectedDate() {
+        return selectedDate;
+    }
+}

9ac19019034f490064dea1c0cf0e45aa8720919f
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-04-06 18:09:34 +0200

    Update OnMyDateChangeListener.java
    add comments

--- app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/Utils/UIHelpers/Components/Calender/OnMyDateChangeListener.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/traveltracker_project/Utils/UIHelpers/Components/Calender/OnMyDateChangeListener.java
@@ -1,5 +1,14 @@
 package fr.univ_poitiers.dptinfo.traveltracker_project.Utils.UIHelpers.Components.Calender;
 
+/**
+ * Interface definition for a callback to be invoked when the date is changed.
+ */
 public interface OnMyDateChangeListener {
+
+    /**
+     * Called when the date is changed.
+     *
+     * @param date The new selected date in the format "dd-MM-yyyy".
+     */
     void onDateChange(String date);
 }
